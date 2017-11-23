5db5f2a070fc4c1903a358031495ec246acc65fd
mateusssss <mateuss10.ms@gmail.com>
2017-11-23 20:45:03 -0200

    Alterado

--- smartfut/rodape.php
+++ smartfut/rodape.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="row bg-light-gray">
 		<div class="col-lg-12">
-			<span> <?php echo $_SESSION["DESCRICAO_PERFIL"] . " - " . $_SESSION["NOME"]; ?> </span>
+			<span> <?php echo $_SESSION["NOME"]; ?> </span>
 			<span class="pull-right"> <?php echo "Login - " . $_SESSION["LOGIN"]; ?> </span>
 		</div>
 	</div>
